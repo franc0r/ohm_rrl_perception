a1605f7fddda4715569a2e09c491c67647f41c36
Christian Pfitzner <christian.pfitzner@evocortex.com>
2018-04-19 22:41:51 +0200

    -- new improvements for robocup 2018

--- ohm_rrl_motiondetection/src/MotionDetectionNode.h
+++ ohm_rrl_motiondetection/src/MotionDetectionNode.h
@@ -8,8 +8,8 @@
 #ifndef MOTIONDETECTIONNODE_H_
 #define MOTIONDETECTIONNODE_H_
 
-#include <ros/ros.h>
-#include <image_transport/image_transport.h>
+#include <ros/ros.h> 
+#include <image_transport/image_transport.h> 
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 
