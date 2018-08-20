ba592639657de7c9510e4a10586322f7863ec08e
Johanna Gleichauf <johanna.gleichauf@th-nuernberg.de>
2018-08-20 14:10:03 +0200

    Adaption C-detection Code clean up

--- ohm_rrl_cdetection/src/auto_c_detect_node.cpp
+++ ohm_rrl_cdetection/src/auto_c_detect_node.cpp
@@ -89,29 +89,48 @@ cv_bridge::CvImagePtr frame;
 
 
 
-void loadSourceImage(cv::Mat& src, std::string filename)
+cv::Mat loadSourceImage(cv::Mat& src, std::string filename) //TODO: src is not written into c_template
 {
   /// Load source image
   std::string img_path = g_path_to_template + filename;
+//  std::cout << "filename " << filename << std::endl;
+  std::cout << "img_path: " << img_path << std::endl;
   src = imread(img_path, 1);
   if(!src.data)                              // Check for invalid input
   {
     cout << "Could not open or find the image " << img_path << std::endl;
   }
+  return src;
 }
 
 
-void loadSourceImage(C_template c_template)
+C_template loadSourceImage(C_template c_template)
 {
-  loadSourceImage(c_template.src, c_template.filename);
+  cv::Mat image;
+  C_template c_temp_filled;
+  c_temp_filled = c_template;
+  image = loadSourceImage(c_template.src, c_template.filename);
+  std::cout << "size c_template " << image.size() << std::endl;
+  c_temp_filled.src = image;
+  std::cout << "name c_template_filled " << c_temp_filled.name << std::endl;
+  std::cout << "size c_template filled " << c_temp_filled.src.cols << std::endl;
+  return c_temp_filled;
 }
 
 
-void loadSourceImages(std::vector<C_template> cs)
-{
-  for(const auto c : cs)
-    loadSourceImage(c);
-}
+//std::vector<C_template> loadSourceImages(std::vector<C_template> cs)
+//{
+//  std::vector<C_template> c;
+//  std::cout << "before whole template is filled " << std::endl;
+//  std::cout << "size cs " << cs.size() << std::endl;
+//  for(unsigned int i=0; i< cs.size(); i++)
+//  {
+//    std::cout << "within for " << i << std::endl;
+//    c[i] = loadSourceImage(cs[i]);
+//    std::cout << "size c 0 " << c[0].src.size() << std::endl;
+//  }
+//  return c;
+//}
 
 
 
@@ -132,29 +151,39 @@ bool templateIsSimilar(C_template c, cv::Moments mu)
 
 void calculateMoments(std::vector<C_template> cs)
 {
+  std::cout << "within calculateMoments v1 " << std::endl;
+  std::cout << "content cs[0] " << cs[0].src.size() << std::endl;
+
   // Get moments for all C-Templates
   for(unsigned int i=0; i<cs.size(); i++)
   {
-    for(unsigned int j=0; i<cs[i].contours.size(); j++)
-      {
-        cs[i].mu[j] = cv::moments(cs[i].contours[i], false);
-      }
+    std::cout << "within first for loop " << std::endl;
+    for(unsigned int j = 0; j < cs[i].contours.size(); j++)
+        cs[i].mu.push_back(cv::moments(cs[i].contours[j], false));
+
   }
+  std::cout << "end of calculateMoments " << std::endl;
 }
 
 std::vector<cv::Moments> calculateMoments(vector<vector<Point>> contours)
 {
+  std::cout << "within caculateMoments v2 " << std::endl;
   std::vector<cv::Moments> mu;
 
   for(unsigned int i = 0; i < contours.size(); i++)
+  {
+    std::cout << "within for loop v2 " << std::endl;
     mu.push_back(cv::moments(contours[i], false));
-
+  }
+  std::cout << "after for loop v2 " << std::endl;
   return mu;
 }
 
 
 bool momentIsSimilar(C_template c, std::vector<cv::Moments> mu, float th = 0.007)
 {
+
+  std::cout << "Within momentIsSimilar " << std::endl;
   for(const auto m : mu)
   {
     for(unsigned int i=0 ; i<c.contours.size() ; ++i)
@@ -171,6 +200,7 @@ bool momentIsSimilar(C_template c, std::vector<cv::Moments> mu, float th = 0.007
 //callback Intel Realsense camera data
 void callCam(const sensor_msgs::ImageConstPtr& camImage)
 {
+  std::cout << "within camera callback " << std::endl;
   try
   {
     frame = cv_bridge::toCvCopy(camImage, camImage->encoding);
@@ -187,31 +217,12 @@ void callCam(const sensor_msgs::ImageConstPtr& camImage)
 
   //Convert _Input to gray
   //RGB Image
+  std::cout << "before cvtColor " << std::endl;
   cvtColor(frame->image, img_gray, CV_RGB2GRAY);
+  std::cout << "after cvtColor " << std::endl;
   blur(img_gray, img_gray, Size(3, 3));
 
   constexpr unsigned int kernel_size = 1;
-
-  //Fill c-template vector
-  for(unsigned int i=0; i<=7; i++)
-    cs.push_back(C_template());
-
-  // Assignment C Templates
-  cs[0].name = "bottom";
-  cs[1].name = "top";
-  cs[2].name = "left";
-  cs[3].name = "right";
-  cs[4].name = "bottom_right";
-  cs[5].name = "bottom_left";
-  cs[6].name = "top_right";
-  cs[7].name = "top_left";
-
-  //Convert C-Template image and blur it
-  for(unsigned int j=0; j<=cs.size(); j++)
-  {
-    cvtColor(cs[j].src, cs[j].gray, CV_BGR2GRAY);
-    blur(cs[j].gray, cs[j].gray, Size(cs[j].kernel_size, cs[j].kernel_size));
-  }
 
   thresh_callback(0, 0);
 }
@@ -227,7 +238,9 @@ int main(int argc, char** argv)
   image_transport::ImageTransport it(nh);
   sensor_msgs::ImagePtr msg;
 
-  private_nh.param("path_to_template", g_path_to_template, std::string(""));
+
+
+  private_nh.param("path_to_template", g_path_to_template, std::string("/home/johanna/catkin/src/ohm_rrl_perception/ohm_rrl_cdetection/images/"));
 
   if(g_path_to_template.length() == 0)
   {
@@ -236,13 +249,53 @@ int main(int argc, char** argv)
   }
 
   //Subscriber Realsense D415
-  image_transport::Subscriber imgSub = it.subscribe("/gripper/color/image_raw", 1, callCam);
+  image_transport::Subscriber imgSub = it.subscribe("/usb_cam/image_raw", 1, callCam); //"/gripper/color/image_raw"
 //  pubGray = it.advertise("/gripper/image_gray", 1);
   pubContours = it.advertise("/gripper/image_contours", 1);
 //  pubDetected = it.advertise("/gripper/detected_contours", 1);
 
-  // Reference images
-  loadSourceImages(cs);
+
+  //Fill c-template vector
+  for(unsigned int i=0; i<=7; i++)
+    cs.push_back(C_template());
+
+  // Assignment C Templates Names
+  cs[0].name = "bottom";
+  cs[1].name = "top";
+  cs[2].name = "left";
+  cs[3].name = "right";
+  cs[4].name = "bottom_right";
+  cs[5].name = "bottom_left";
+  cs[6].name = "top_right";
+  cs[7].name = "top_left";
+
+  // Assignment C Templates filenames
+  cs[0].filename = "C_gross_unten.jpg" ;
+  cs[1].filename = "C_gross_oben.jpg" ;
+  cs[2].filename = "C_gross_links.jpg" ;
+  cs[3].filename = "C_mittel_rechts.jpg" ;
+  cs[4].filename = "C_gross_rechts_unten.jpg" ;
+  cs[5].filename = "C_gross_links_unten.jpg" ;
+  cs[6].filename = "C_gross_rechts_oben.jpg" ;
+  cs[7].filename = "C_gross_links_oben.jpg" ;
+
+  std::cout << "before templates are loaded " << std::endl;
+
+  //Convert C-Template image and blur it
+  for(unsigned int j=0; j<cs.size(); j++)
+  {
+    //Load templates
+    cs[j] = loadSourceImage(cs[j]);
+    std::cout << "size cs.src " << cs[j].src.size() << std::endl;
+    std::cout << "before cvtColor " << j << std::endl;
+    cvtColor(cs[j].src, cs[j].gray, CV_BGR2GRAY);
+    std::cout << "after cvtColor " << j << std::endl;
+    blur(cs[j].gray, cs[j].gray, Size(cs[j].kernel_size, cs[j].kernel_size));
+    std::cout << "after blur " << j << std::endl;
+  }
+
+ std::cout << "templates loaded and blurred " << std::endl;
+
 
   ros::spin();
 }
@@ -250,14 +303,16 @@ int main(int argc, char** argv)
 /** @function thresh_callback */
 void thresh_callback(int, void*)
 {
+  std::cout << "within callback thresh " << std::endl;
   Mat canny_output;
 
   vector<vector<Point> > contours;
   vector<Vec4i> hierarchy;
 
   //Edge detection and find contours for all C-Templates
-  for(unsigned int k=0; k<=cs.size(); k++)
+  for(unsigned int k=0; k<cs.size(); k++)
   {
+    std::cout << "within edge detection " << std::endl;
     Canny(cs[k].gray, cs[k].canny, thresh, thresh * 2 , 3);
     findContours(cs[k].canny, cs[k].contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
   }
@@ -265,6 +320,10 @@ void thresh_callback(int, void*)
   //RGB Image
   Canny(img_gray, canny_output, thresh, thresh * 2, 3);
   findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+  std::cout << "before calculation of moments " << std::endl;
+
+  std::cout << "content cs[0] " << cs[0].src.size() << std::endl;
 
   // Get the moments
   calculateMoments(cs);
@@ -286,7 +345,7 @@ void thresh_callback(int, void*)
 //          && (abs(mu[i].nu02 - cs[k].mu[j].nu02) < 0.007) && (abs(mu[i].nu30 - cs[k].mu[j].nu30) < 0.007)
 //          && (abs(mu[i].nu21 - cs[k].mu[j].nu21) < 0.007) && (abs(mu[i].nu12 - cs[k].mu[j].nu12) < 0.007)
 //          && (abs(mu[i].nu03 - cs[k].mu[j].nu03) < 0.007))
-      if(momentIsSimilar(cs[k], mu)) g_detected_contours.push_back(contours[i]);
+      if(momentIsSimilar(cs[k], mu)) g_detected_contours.push_back(contours[i]);  //TODO: check Code with actual C's
     }
   }
 
